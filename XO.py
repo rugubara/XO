a1289c76453dd2a2dce8a9067b3cc516c1445df0
Anton Gubarkov <anton.gubarkov@gmail.com>
2023-11-26 13:11:36 +0300

    refactored XO.py to be used as a module

--- XO.py
+++ XO.py
@@ -3,69 +3,65 @@
 import pygame
 import pygame.colordict
 import random
-import numpy as np
-import cell
-import pfModel
+import pygame_gui
 import playfield
 
 
+class XOApp:
+    def __init__(self):
+        pygame.init()
+        random.seed()
+
+        # Set the width and height of the screen [width, height]
+        size = 703
+        g = int(size / 19)
+        c = 5 * g
+        print(c, g)
+        self.screen = pygame.display.set_mode([size] * 2)
+        pygame.font.init()
+        pygame.display.set_caption("Крестики Нолики - ход Х")
+        self.pf = playfield.PlayField(self.screen.get_size(), g, c)
+
+        return
+    def run(self):
+        # Loop until the user clicks the close button.
+        done = False
+        clock = pygame.time.Clock()
+
+        # -------- Main Program Loop -----------
+        while not done:
+            # --- Main event loop
+            for event in pygame.event.get():
+                # print(event)
+                self.pf.dispatch(event)
+                if event.type == pygame.QUIT:
+                    done = True
 
 
 
+            # --- Drawing code should go here
+            self.pf.draw(self.screen)
+            # --- Go ahead and update the screen with what we've drawn.
+            pygame.display.flip()
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+            # --- Limit to 10 frames per second
+            clock.tick(10)
+        return
+    def createUI(self):
+        return
 
-pygame.init()
-random.seed()
 
-# Set the width and height of the screen [width, height]
-size = 703
-g = int(size / 19)
-c = 5 * g
-print(c, g)
-screen = pygame.display.set_mode([size] * 2)
-pygame.font.init()
+# # Define some colors
+# BLACK = (0, 0, 0)
+# WHITE = (255, 255, 255)
+# GREEN = (0, 255, 0)
+# RED = (255, 0, 0)
 
-pygame.display.set_caption("Крестики Нолики - ход Х")
+if __name__ == '__main__':
+    app = XOApp()
+    app.run()
+    pygame.quit()
 
-# Loop until the user clicks the close button.
-done = False
 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-pf = playfield.PlayField((size, size), g, c)
-
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        # print(event)
-        pf.dispatch(event)
-        if event.type == pygame.QUIT:
-            done = True
-
-    # --- Game logic should go here
-
-    # --- Screen-clearing code goes here
-
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-    # screen.fill(WHITE)
-
-    # --- Drawing code should go here
-    pf.draw(screen)
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    # --- Limit to 10 frames per second
-    clock.tick(10)
 
 # Close the window and quit.
-pygame.quit()
